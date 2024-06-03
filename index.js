a446c6fa2ab18191df00c4752e5b8ce70ca552a4
ananya-sh <95671410+ananya-sh@users.noreply.github.com>
2024-06-03 17:46:37 +0530

    Update index.js
    updated dice roll

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 document.addEventListener("keydown",function(event){
     if(event.key==="Enter"){
-            var randomNumber1 = Math.floor(Math.random()*6 +1);
-document.querySelector(".img1").setAttribute("src", "images/dice"+randomNumber1+".png");
+        var randomNumber1 = Math.floor(Math.random()*6 +1);
+    document.querySelector(".img1").setAttribute("src", "images/dice"+randomNumber1+".png");
 
-var randomNumber2 = Math.floor(Math.random()*6 +1);
-document.querySelector(".img2").setAttribute("src", "images/dice"+randomNumber2+".png");
+    var randomNumber2 = Math.floor(Math.random()*6 +1);
+    document.querySelector(".img2").setAttribute("src", "images/dice"+randomNumber2+".png");
 
-if(randomNumber1>randomNumber2){
-        document.querySelector("h1").textContent="Player 1 wins!!";
+    if(randomNumber1>randomNumber2){
+            document.querySelector("h1").textContent="Player 1 wins!!";
+        }
+        else if(randomNumber2>randomNumber1){
+            document.querySelector("h1").textContent="Player 2 wins!!";
+        }
+        else{
+            document.querySelector("h1").textContent="Draw :)";
+        }    
     }
-    else if(randomNumber2>randomNumber1){
-        document.querySelector("h1").textContent="Player 2 wins!!";
-    }
-    else{
-        document.querySelector("h1").textContent="Draw :)";
-    }
-            
-    })
-        
+})
 
 
