4b2936155c35d2a8cee55ec264b3da4a6ec3b364
ananya-sh <95671410+ananya-sh@users.noreply.github.com>
2024-06-03 17:37:40 +0530

    Update index.js
    user can roll dice on clicking enter

--- index.js
+++ index.js
@@ -1,64 +1,6 @@
-// var randomNumber1 = Math.random()*6 + 1;
-// randomNumber1 = Math.floor(randomNumber1);
-
-// // for image 2
-// if(randomNumber1===1){
-//     document.querySelector(".img1").setAttribute("src", "images/dice1.png");
-// }
-// else if(randomNumber1===2){
-//     document.querySelector(".img1").setAttribute("src", "images/dice2.png");
-// }
-// else if(randomNumber1===3){
-//     document.querySelector(".img1").setAttribute("src", "images/dice3.png");
-// }
-// else if(randomNumber1===4){
-//     document.querySelector(".img1").setAttribute("src", "images/dice4.png");
-// }
-// else if(randomNumber1===5){
-//     document.querySelector(".img1").setAttribute("src", "images/dice5.png");
-// }
-// else if(randomNumber1===6){
-//     document.querySelector(".img1").setAttribute("src", "images/dice6.png");
-// }
-
-// // for image 2
-
-// var randomNumber2 = Math.random()*6 + 1;
-// randomNumber2 = Math.floor(randomNumber2);
-
-// if(randomNumber2===1){
-//     document.querySelector(".img2").setAttribute("src", "images/dice1.png");
-// }
-// else if(randomNumber2===2){
-//     document.querySelector(".img2").setAttribute("src", "images/dice2.png");
-// }
-// else if(randomNumber2===3){
-//     document.querySelector(".img2").setAttribute("src", "images/dice3.png");
-// }
-// else if(randomNumber2===4){
-//     document.querySelector(".img2").setAttribute("src", "images/dice4.png");
-// }
-// else if(randomNumber2===5){
-//     document.querySelector(".img2").setAttribute("src", "images/dice5.png");
-// }
-// else if(randomNumber2===6){
-//     document.querySelector(".img2").setAttribute("src", "images/dice6.png");
-// }
-
-// // comparing the score
-
-// if(randomNumber1>randomNumber2){
-//     document.querySelector("h1").textContent="Player 1 wins!!";
-// }
-// else if(randomNumber2>randomNumber1){
-//     document.querySelector("h1").textContent="Player 2 wins!!";
-// }
-// else{
-//     document.querySelector("h1").textContent="Draw :)";
-// }
-
-
-var randomNumber1 = Math.floor(Math.random()*6 +1);
+document.addEventListener("keydown",function(event){
+    if(event.key==="Enter"){
+            var randomNumber1 = Math.floor(Math.random()*6 +1);
 document.querySelector(".img1").setAttribute("src", "images/dice"+randomNumber1+".png");
 
 var randomNumber2 = Math.floor(Math.random()*6 +1);
@@ -73,3 +15,8 @@ if(randomNumber1>randomNumber2){
     else{
         document.querySelector("h1").textContent="Draw :)";
     }
+            
+    })
+        
+
+
